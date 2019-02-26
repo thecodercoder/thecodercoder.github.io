f6e9c765aff2834af9dffe78da4be7f939b9d746
Jessica <>
2019-02-26 16:45:37 -0600

    Updated Links page

--- links/script.e0d5003c.js
+++ links/script.e0d5003c.js
@@ -207,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58485" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56161" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
